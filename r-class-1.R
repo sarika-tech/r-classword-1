12b1ca68cc8b9a96d8bd6bbfb72044cd29408be8
sarika-tech <79010904+sarika-tech@users.noreply.github.com>
2021-02-25 16:08:18 +0530

    Create r-class-1.R

---
+++ r-class-1.R
@@ -0,0 +1,19 @@
+print ("Hello, World!")
+2+3
+(2+3)
+a=2
+b<-19
+nummy <- c (2,3, 4)
+nummy_int <- c(1L, 2L, 3L)
+typeof(nummy)
+typeof(nummy_int)
+is.numeric (nummy)
+is.numeric(nummy_int)
+logicals <- c(2,3,4)
+money_in_chairs <- c ("20", "35", "33")
+money_money <- as.numeric (money_in_chairs)
+c (2, 4, "hello", TRUE)
+c (2, 4, TRUE)
+month.abb
+month.abb[c(7,8,12)]
+days <- c("Mon", "Tue", "Wed")
